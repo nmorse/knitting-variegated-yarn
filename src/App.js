df90300fb44d659a9d08a163ef24f9487b7f1f44
nmorse <morse.nate@gmail.com>
2018-11-26 00:36:34 +0000

    swapped while loop for 'for' and simplified the CheckBox

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class Patch extends Component {
     this.state = {
       backieForthie: false,
       stitchWidth: 2,
-      skane: ['#F00FFF', '#F00FFF', '#F00FFF', '#F00FFF', '#FFF00F', '#FFF00F', '#FFF00F', ' hsl(120,30%,30%)']
+      skane: ['#F00FFF', '#F00FFF', '#F00FFF', '#F00FFF', '#FFF00F', '#FFF00F', '#FFF00F', ' hsl(120,100%,30%)']
       //      skane: ['#F00FFF', '#FFF00F', '#F00F00', '#00FFF0', '#FF000F', '#0FF0F0', '#000FFF', '#0F000F', '#F0F0FF']
     };
     this.resizePatch = this.resizePatch.bind(this); // fix the access of the wrong 'this'
@@ -38,12 +38,10 @@ class Patch extends Component {
 
   renderStitchRow(row) {
     let dynamic_stitches = [];
-    let sw = this.state.stitchWidth;
-    let i = row * sw;
-    while (sw > 0) {
-      dynamic_stitches.push(<Stitch column={sw} color={this.getColor(i)} />);
-      i += 1;
-      sw -= 1;
+    const sw = this.state.stitchWidth;
+    const start_index = row * sw;
+    for (let stitch_index = start_index; stitch_index < start_index + sw; stitch_index++) {
+      dynamic_stitches.push(<Stitch key={stitch_index} color={this.getColor(stitch_index)} />);
     }
     if (this.state.backieForthie && (row % 2) === 1) {
       dynamic_stitches = dynamic_stitches.reverse();
@@ -68,14 +66,14 @@ class Patch extends Component {
   render() {
     return (
       <div >
-          {this.renderStitchRow(0)}
-          {this.renderStitchRow(1)}
-          {this.renderStitchRow(2)}
-          {this.renderStitchRow(3)}
-          {this.renderStitchRow(4)}
-          {this.renderStitchRow(5)}
-          {this.renderStitchRow(6)}
-          {this.renderStitchRow(7)}
+        {this.renderStitchRow(0)}
+        {this.renderStitchRow(1)}
+        {this.renderStitchRow(2)}
+        {this.renderStitchRow(3)}
+        {this.renderStitchRow(4)}
+        {this.renderStitchRow(5)}
+        {this.renderStitchRow(6)}
+        {this.renderStitchRow(7)}
         <Resizer startX={20} label={'<-- ' + this.state.stitchWidth + ' stitches to cast on -->'} onClick={this.resizePatch} />
         <CheckBox label="backie forthie" uniqueId="bfcb" onChange={this.updateBackForth} />
       </div>
@@ -121,25 +119,35 @@ class Resizer extends Component {
 }
 
 
-class CheckBox extends Component {
-  render() {
-    return (
-      <span>
-        <input id={this.props.uniqueId} type="checkbox"
-          onChange={(e) => this.props.onChange(e.target.checked)}
-        />
-        <label htmlFor={this.props.uniqueId}> {this.props.label} </label>
-      </span>
-    );
-  }
+// class CheckBox extends Component {
+//   render() {
+//     return (
+//       <span>
+//         <input id={this.props.uniqueId} type="checkbox"
+//           onChange={(e) => this.props.onChange(e.target.checked)}
+//         />
+//         <label htmlFor={this.props.uniqueId}> {this.props.label} </label>
+//       </span>
+//     );
+//   }
+// }
+function CheckBox(props) {
+  return (
+    <span>
+      <input id={props.uniqueId} type="checkbox"
+        onChange={(e) => props.onChange(e.target.checked)}
+      />
+      <label htmlFor={props.uniqueId}> {props.label} </label>
+    </span>
+  );
 }
-
 
 class App extends Component {
   render() {
     return (
-      <div className="knit">
-        <div className="knit-patch">
+      <div>
+        <h2 className="header" > Knitting Viz </h2>
+        <div className="knit">
           <Patch />
         </div>
         <div className="knit-info">
